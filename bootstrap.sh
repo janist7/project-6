32257c7da9eb1939ece06a5089b271557286cc66
Janis Tidrikis <janis.tidrikis@delfi.lv>
2017-08-21 15:49:11 +0300

    Improvements to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,39 @@
 apt-get update
 apt-get upgrade
-apt-get install memcached
+apt-get -qqy install memcached
 apt-get -qqy install redis-server
-apt-get -qqy install make zip unzip postgresql python-psycopg2 libpq-dev
-apt-get -qqy install python3 python3-pip
+apt-get -qqy install make
+apt-get -qqy install zip
+apt-get -qqy install unzip
+apt-get -qqy install postgresql
+apt-get -qqy install pgadmin3
+apt-get -qqy install python-psycopg2
+apt-get -qqy install libpq-dev
 apt-get -qqy install python-dev
 apt-get -qqy install nodejs
 apt-get -qqy install npm
 npm install bower -g
-pip3 install --upgrade pip
-pip3 install flask packaging oauth2client redis passlib flask-httpauth
-pip3 install sqlalchemy flask-sqlalchemy psycopg2 bleach requests
-apt-get -qqy install python python-pip
+#apt-get -qqy install python3
+#apt-get -qqy install python3-pip
+#pip3 install --upgrade pip
+#pip3 install flask packaging oauth2client redis passlib flask-httpauth
+#pip3 install sqlalchemy flask-sqlalchemy psycopg2 bleach requests
+apt-get -qqy install python
+apt-get -qqy install python-pip
 pip2 install --upgrade pip
-pip2 install flask packaging oauth2client redis passlib flask-httpauth
-pip2 install sqlalchemy flask-sqlalchemy psycopg2 bleach requests
+pip2 install flask
+pip2 install packaging
+pip2 install oauth2client
+pip2 install redis
+pip2 install passlib
+pip2 install flask-httpauth
+pip2 install sqlalchemy
+pip2 install flask-sqlalchemy
+pip2 install psycopg2
+pip2 install bleach
+pip2 install requests
 apt-add-repository ppa:brightbox/ruby-ng
 apt-get install -y ruby2.3 ruby2.3-dev libsqlite3-dev
 gem install mailcatcher
-echo 'if [ -n "$BASH_VERSION" ]; then
-		if [ -f "/vagrant/*/.env" ]; then
-        	. "/vagrant/*/.env"
-    	fi
-	fi' >> ~/.profile
-source /vagrant/*/.env
-export $(cut -d= -f1 /vagrant/*/.env)
+echo "source /vagrant/*/.env" >> ~/.profile
+echo "export $(cut -d= -f1 /vagrant/*/.env)" >> ~/.profile
