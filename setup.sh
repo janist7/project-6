0e9ceae4e0c53185ea593f8bd82e482cda9d6ed4
Janis Tidrikis <janis.tidrikis@delfi.lv>
2017-09-19 11:47:14 +0300

    Changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-apt-add-repository ppa:brightbox/ruby-ng -y
-apt-get update -y
-apt-get upgrade -y
+apt-get -qqy update
+apt-get -qqy upgrade
 apt-get -qqy install memcached
 apt-get -qqy install redis-server
 apt-get -qqy install make
@@ -15,15 +14,18 @@ apt-get -qqy install python-dev
 apt-get -qqy install nodejs
 apt-get -qqy install npm
 apt-get -qqy  install build-essential
-apt-get -qqy  install -y ruby2.4.2 ruby2.4.2-dev libsqlite3-dev
+apt-add-repository ppa:brightbox/ruby-ng -y
+apt-get -qqy update
+apt-get -qqy install ruby2.3 ruby2.3-dev libsqlite3-dev
+apt-get -qqy install python
+apt-get -qqy install python-pip
 npm install bower -g
+gem install -q mailcatcher
 #apt-get -qqy install python3
 #apt-get -qqy install python3-pip
 #pip3 install --upgrade pip
 #pip3 install flask packaging oauth2client redis passlib flask-httpauth
 #pip3 install sqlalchemy flask-sqlalchemy psycopg2 bleach requests
-apt-get -qqy install python
-apt-get -qqy install python-pip
 pip2 install --upgrade pip
 pip2 install flask
 pip2 install packaging
@@ -38,8 +40,8 @@ pip2 install bleach
 pip2 install requests
 cd /vagrant/*/
 apt-get -qqy install language-pack-UTF-8
+locale-gen UTF-8
 ln -s /usr/bin/nodejs /usr/bin/node
-sudo gem install mailcatcher
 make init
 make assets
 make clean
